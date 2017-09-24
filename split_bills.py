5cb4ce6fd1a3e901a3bf8c904cbf8528cf25155e
Arjun Menon <ajn.menon@gmail.com>
2017-09-24 16:26:40 -0700

    Cleanup
    - make python3 compatible
    - add some comments for clarity
    TODO: code is still a mess

--- split_bills.py
+++ split_bills.py
@@ -1,5 +1,8 @@
+# returns a tuple summarizing the receipt of the transactions:
+# 0: tip            the flat dollar amount of tip added to the bill
+# 1: transactions   list of dicts (of price, and participant list)
 def parse_transactions(lines):
-    tip = 0
+    tip = 0.0
     transactions = [];
 
     for line in lines:
@@ -17,21 +20,29 @@ def parse_transactions(lines):
     return (transactions, tip)
     
 def get_baked_list():
-  ret = """14.12,!tip
-  11.7,person1
-  21.24,person2
-  20.34,person3"""
+  ret = """7.51,!tip
+  14.95,p1
+  13.95,p2
+  12.95,p3
+  3.95,p3
+  3.5,p2"""
   return ret.split('\n')
 
+# for each item in the transaction, compute the item_price_per_participant
+#  if it was split evenly between all participants
+# then for each participant, add together all item_price_per_participant 
+#  that they are a part of
 def compute_subtotals(transactions):
-    subtotals = {}
+    subtotals_per_participant = {}
     for transact in transactions:
-        num_parts = len(transact['participants'])
-        cost_per_part = transact['price'] / num_parts
+        num_participants_per_item = len(transact['participants'])
+        cost_per_participants = transact['price'] / num_participants_per_item
         for part in transact['participants']:
-            subtotals[part] = cost_per_part + subtotals.get(part,0.0)
-    return subtotals
+            subtotals_per_participant[part] = cost_per_participants + subtotals_per_participant.get(part,0.0)
+    return subtotals_per_participant
 
+# given a list of subtotals per participant,
+# compute a proportional split of the common item's price (tax, tip, etc)
 def compute_contribution(subtotals, common):
     total = sum(subtotals.values())
     subcontribs = {part: common*subtotal/total for (part,subtotal) in subtotals.items()}
@@ -44,15 +55,22 @@ def main():
     subtotal = sum(subtotal_pp.values())
 
     # tax_pp is (subtotal_pp / subtotal_all) * tax_total
-    tax_total = subtotal * (8.75/100.0)
+    tax_total = subtotal * (8.5/100.0)
+    
+    print('{:9} = {:04.2f}'.format('subtotal',subtotal))
+    print('{:9} = {:04.2f}'.format('tax',     tax_total))
+    print('{:9} = {:04.2f}'.format('total',   tax_total+subtotal))
+    
     tax_pp = compute_contribution(subtotal_pp, tax_total)
     
     tip_pp = compute_contribution(subtotal_pp, tip_total)
 
-    # for each participant, final amount owed is the sum of subtotal, tax, tip
-    for part in subtotal_pp.keys():
-        amount = subtotal_pp[part] + tax_pp[part] + tip_pp[part]
-        print '{0} owes {1}'.format(part, amount)
+    # for each participant, final amount owed is the 
+    #  sum of subtotal, tax, tip (as calculated per participant)
+    print()
+    print('Result:')
+    for participant in subtotal_pp.keys():
+        amount = subtotal_pp[participant] + tax_pp[participant] + tip_pp[participant]
+        print('{:7} owes {:04.2f}'.format(participant, amount))
 
-if __name__=="__main__":
-    main()
+if __name__=="__main__": main()
